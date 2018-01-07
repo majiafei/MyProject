e787174fa4b4d5e30cb538e913fdd06640d393fd
Caffee <157121458@q.com>
2018-01-07 10:05:25 +0800

    df

---
+++ src/Master.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+
+public class Master {
+	
+	public static void main(String[] args) {
+		System.out.println(System.currentTimeMillis());
+		Vector vector = new Vector<>();
+		for (int i = 0; i < 25; i++) {
+			byte[] b = null;
+			b = new byte[1024*1024];
+			vector.add(b);
+		}
+		System.out.println(System.currentTimeMillis());
+	}
+
+}
